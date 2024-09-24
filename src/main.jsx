34cbdf0c72febfc4fef97ee5c02ed9df09c57f3e
Jeevitha <ravikumarjeevitha254@gmail.com>
2024-09-24 19:20:45 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import App from './App.jsx'
-//import './index.css'
-import './QrCode.css'
+import './index.css'
+//import './QrCode.css'
 //import { QrCode } from './QrCode'
 import { UserCard } from './UserCard.jsx'
 
